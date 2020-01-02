a28baa6ba05be338b5ffa425c489a3e38766f4f9
likun1993630 <sdlclk93@gmail.com>
2020-01-03 00:04:43 +0100

    add

--- tcp/compile.sh
+++ tcp/compile.sh
@@ -1,6 +1,7 @@
 g++ -std=c++11 -g server.cpp -o tcp_server -lboost_system
 g++ -std=c++11 -g client.cpp -o tcp_client -lboost_system
 g++ -std=c++11 -g server_big.cpp -o tcp_server_big -lboost_system
+g++ -std=c++11 -g server_big_2209.cpp -o tcp_server_big_2209 -lboost_system
 g++ -std=c++11 -g server_big_2205.cpp -o server_big_2205 -lboost_system
 g++ -std=c++11 -g server_big_2209+others.cpp -o server_big_2209+others -lboost_system
 
